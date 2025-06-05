bebad8d5253882572ac917563e64d0f3f0030d0c
CeereIngeniero1 <soporte2ceere@gmail.com>
2025-06-05 08:38:58 -0500

    Fernando

---
+++ ServerFer.js
@@ -0,0 +1,46 @@
+const { Request, TYPES } = require('tedious');
+const Router = require('express').Router;
+const connection = require('db');
+
+const router = Router();
+
+
+router.get('BuscarFactura/:IdEmpresaV/:NumeroFactura', async (req, res) => {
+
+    const IdEmpresaV = req.params.IdEmpresaV;
+    const NumeroFactura = req.params.NumeroFactura;
+
+    const request = new Request(
+        `
+        SELECT FC.[Id Factura] FROM Factura FC
+        WHERE FC.[Id EmpresaV] = @IdEmpresaV  AND FC.[No Factura] = @NumeroFactura
+        `,
+        (err) => {
+            if (err) {
+                console.error('Error executing patient query:', err);
+                res.status(500).send('Internal Server Error');
+            }
+        }
+
+    );
+
+    request.addParameter('IdEmpresaV', TYPES.Int, IdEmpresaV);
+    request.addParameter('NumeroFactura', TYPES.VarChar, NumeroFactura);
+
+    let idfactura;
+
+    request.on('row', (columns) => {
+        idfactura = columns[0];
+    })
+
+    request.on('requestCompleted', async () => {
+        idfactura
+    }
+
+    )
+
+});
+
+
+
+
