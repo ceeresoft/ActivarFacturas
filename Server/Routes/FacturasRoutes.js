0909d0dae1dff8ba68c962dfb1bc289df5858c14
CeereIngeniero1 <soporte2ceere@gmail.com>
2025-07-09 11:05:05 -0500

    listo sebas

--- Server/Routes/FacturasRoutes.js
+++ Server/Routes/FacturasRoutes.js
@@ -4,6 +4,11 @@ const connection = require('../bd/bd');
 
 const router = Router();
 
+
+router.get('/', (req, res) => {
+    res.status(200).send('Todo esta ok');
+})
+
 router.get('/PruebaFactura', async (req, res) => {
     try {
         const request = new Request(
@@ -19,9 +24,36 @@ router.get('/PruebaFactura', async (req, res) => {
 
         let resultados = [];
 
-        require.on
+        request.on('row', (Columns) => {
+            let factura = {
+                idfactura: Columns[0].value
+            };
+            resultados.push(factura);
+        });
+
+         request.on('requestCompleted', () => {
+            console.log('Resultados de la consulta:');
+            console.log(resultados);
+            if (!res.headersSent) {
+                res.json(resultados);  
+            }
+        });
+
+        request.on('error', (err) => {
+            console.error('Error en la consulta:', err);
+            if (!res.headersSent) {
+                res.status(500).send('Error interno del servidor');
+            }
+        });
+
+        connection.execSql(request);
 
     } catch (error) {
-        
+        console.error('Error en la conexión o en la ejecución de la consulta:', error);
+        if (!res.headersSent) {
+            res.status(500).send('Error interno del servidor');
+        }
     }
 });
+
+module.exports = router;
--- Server/Server.js
+++ Server/Server.js
@@ -12,7 +12,7 @@ app.use(compression());
 
 app.use(cors());
 
-app.use(express-json({limit: '1000mb'}));
+app.use(express.json({limit: '1000mb'}));
 
 app.use(express.urlencoded({ limit: '1000mb', extended: true}));
 
